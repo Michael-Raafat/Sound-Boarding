79f1ffe8d56a7ee8c47936c14dfe8b333688f32b
Michael <micoraafat96@gmail.com>
2017-08-02 17:08:00 +0200

    Sound Boarding project

---
+++ app/src/main/java/studios/kdc/soundboarding/MainActivity.java
@@ -0,0 +1,13 @@
+package studios.kdc.soundboarding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
